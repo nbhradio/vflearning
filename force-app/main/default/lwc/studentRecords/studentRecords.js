25b44a4474f581c5824397abba432d7de6637096
nbhradio <kulshrestha.sid@gmail.com>
2019-10-08 20:22:09 +0530

    First Commit

---
+++ force-app/main/default/lwc/studentRecords/studentRecords.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import { track } from 'lwc';
+
+export default class StudentRecords extends LightningElement {
+
+    @track studentName = 'student_name';
+    
+}    
